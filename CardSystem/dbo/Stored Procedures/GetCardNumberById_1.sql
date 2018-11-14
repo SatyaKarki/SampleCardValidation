352cdf5ac6d9469689c1e76a7c71aaa56b2dac92
satya <satya@nobleidea.com>
2018-11-14 23:38:41 +0600

    Database project added with schema and sp

---
+++ CardSystem/dbo/Stored Procedures/GetCardNumberById_1.sql
@@ -0,0 +1,8 @@
+﻿
+
+CREATE PROCEDURE [dbo].[GetCardNumberById] 	
+	@Id bigint
+AS
+BEGIN
+	SELECT * From CardNumber WITH(NOLOCK) WHERE Id=@Id;
+END
---
+++ CardSystem/dbo/Tables/CardNumber_1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[CardNumber] (
+    [Id]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [CardTypeId] BIGINT        NOT NULL,
+    [CNumber]    NVARCHAR (50) NULL,
+    [Expiry]     NVARCHAR (6)  NULL,
+    [Active]     BIT           CONSTRAINT [DF_CardNumber_Active] DEFAULT ((0)) NOT NULL,
+    [Created]    DATETIME      CONSTRAINT [DF_CardNumber_Created] DEFAULT (getdate()) NOT NULL,
+    [Author]     BIGINT        NOT NULL,
+    [Modified]   DATETIME      CONSTRAINT [DF_CardNumber_Modified] DEFAULT (getdate()) NOT NULL,
+    [Editor]     BIGINT        NOT NULL,
+    CONSTRAINT [PK_CardNumber] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_CardNumber_CardType] FOREIGN KEY ([CardTypeId]) REFERENCES [dbo].[CardType] ([Id])
+);
+
